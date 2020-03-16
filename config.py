eee200a216f5b45aaede387351b0d5ad45e33318
Nicholas Law Cheng Xue <nicholaslaw@Nicholass-MBP.gateway.2wire.net>
2020-03-16 14:27:09 +0800

    Edited docker compose

--- config.py
+++
@@ -1,6 +0,0 @@
-test_configs = {
-    "label_prob_path": "./data/test_data.csv",
-    "true_labels_field": "label",
-    "label_idx_dic_path": "./data/label2idx.p",
-    "results_folder": "./ROC/"
-}
